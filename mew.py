bebfa99c746467ba8f40939b44397767bc208291
sapienskid <savinpokharel@gmail.com>
2025-12-22 16:57:28 +0545

    create a function to create random matrix

--- mew.py
+++ mew.py
@@ -3,9 +3,17 @@ class MEW:
         self.key_size = key_size
         self.km1 = self._generate_key_matrix()
         self.km2 = self._generate_key_matrix()
-        
+
     def _generate_key_matrix(self):
-        pass
+        """Generate a key_size x key_size matrix with random bytes (0-255)"""
+        matrix = []
+        for i in range(self.key_size):
+            row = []
+            for j in range(self.key_size):
+                row.append(secrets.randbelow(256))
+            matrix.append(row)
+        return matrix
+
     def _get_move(self,byte_val):
         pass
     def _move(self):
